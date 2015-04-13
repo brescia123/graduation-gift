d7cc7e7151818359191f128960967e135e58ed9d
Giacomo Bresciani <brescia123@gmail.com>
2015-04-14 01:34:28 +0200

    ready

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function() {
+    $('#bgvidisney').click(function() {
+        if (this.paused == false) {
+            $('#clickme').show();
+            this.pause();
+        } else {
+            $('#clickme').hide();
+            this.play();
+        }
+    });
+    $('#clickme').click(function() {
+    	    var video = $('#bgvidisney')[0];
+        if (video.paused == false) {
+            $('#clickme').show();
+            video.pause();
+        } else {
+			video.play();
+            $('#clickme').hide();
+        }
+    });
+});
